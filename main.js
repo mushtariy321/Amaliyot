ca5dd4033f560e4a1f47233b627f8649208e59eb
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-05-13 14:48:41 +0300

    done

---
+++ main.js
@@ -0,0 +1,50 @@
+// 1
+// let num = +prompt(`Son kriting!`)
+// let num2 = +prompt(`Yana son kriting!`)
+// alert(num + num2)
+
+//2
+
+// let a = 12
+// console.log(typeof a);
+
+//3
+
+// let number = (Math.floor(Math.random() * 100))
+// alert(`Random son: ${number}`)
+// if (number % 2 === 0) {
+//     alert(`even`);
+// }
+// else {
+//     alert(`odd`)
+// }
+
+//4
+
+// let nam = prompt(`So'z kiriting!`)
+// if (nam === `Mars`){
+//     alert(`Mars`)
+// }
+// else{
+//     alert(`Mars IT School`)
+// }
+
+//5
+
+// let arr = [`dars`, `maktab`, `korea`, `Diyora`, `ko'zyoshlar qirolichasi`]
+// let words = []
+// let word = []
+// for (let word of arr) {
+//     let res = 
+//     }
+
+
+//6
+for (let i = 0; i < 101; i++) {
+    if (str % 2 === 0) {
+        alert(`EVEN`)
+    }
+    else {
+        alert(`ODD`)
+    }
+}
